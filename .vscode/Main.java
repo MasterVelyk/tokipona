2ef9299ce38e257d3f3debfd4cf67945f710b0ab
MasterVelyk <themastersoliday@gmail.com>
2024-04-15 08:47:00 -0400

    Co-authored-by: jan Niki <1Nicky8523@users.noreply.github.com>

--- .vscode/Main.java
+++ .vscode/Main.java
@@ -1,5 +1,31 @@
-public class Main {
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferStrategy;
+import javax.swing.*;
+import java.util.ArrayList;
+public class Main extends Canvas implements KeyListener  {
+    public void keyPressed(KeyEvent e) {
+
+     }
+     public void keyTyped(KeyEvent key) {
+
+     }
+     public void keyReleased(KeyEvent key) {
+     
+     }
+    public Main() {
+        JFrame frame = new JFrame("mah pona");
+        frame.add(this);
+        frame.setSize(1000,750);
+        frame.getContentPane().setBackground(Color.CYAN);
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.addWindowListener(
+           new WindowAdapter() { 
+              public void windowClosing(WindowEvent e) {System.exit(0);} 
+           });
+    }
     public static void main(String[] args) throws Exception {
-    System.out.println("toki a!!!!");
+        new Main();
     }
 }
