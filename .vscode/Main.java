f11b49b97d86b0e9d72b79a59d44a0fc1f349fdd
MasterVelyk <themastersoliday@gmail.com>
2024-04-16 18:24:51 -0400

    This is a commit message

--- .vscode/Main.java
+++ .vscode/Main.java
@@ -3,28 +3,50 @@ import java.awt.event.*;
 import java.awt.image.BufferStrategy;
 import javax.swing.*;
 import java.util.ArrayList;
-public class Main extends Canvas implements KeyListener  {
+
+@SuppressWarnings("unused")
+public class Main extends Canvas implements KeyListener {
+    private static int SCREEN_WIDTH = 1000;
+    private static final int SCREEN_HEIGHT = 750;
+    private static boolean notebookOpen = false;
+
     public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_X) {
+            notebookOpen = true;
+        }
+    }
 
-     }
-     public void keyTyped(KeyEvent key) {
+    public void keyTyped(KeyEvent key) {
 
-     }
-     public void keyReleased(KeyEvent key) {
-     
-     }
+    }
+
+    public void keyReleased(KeyEvent key) {
+
+    }
+
     public Main() {
         JFrame frame = new JFrame("mah pona");
         frame.add(this);
-        frame.setSize(1000,750);
+        frame.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
         frame.getContentPane().setBackground(Color.CYAN);
         frame.setVisible(true);
         frame.setResizable(false);
         frame.addWindowListener(
-           new WindowAdapter() { 
-              public void windowClosing(WindowEvent e) {System.exit(0);} 
-           });
+                new WindowAdapter() {
+                    public void windowClosing(WindowEvent e) {
+                        System.exit(0);
+                    }
+                });
     }
+
+    public void paint(Graphics g) {
+        if (notebookOpen == true) {
+            g.setColor(Color.MAGENTA);
+            g.fillRect(SCREEN_WIDTH / 2 - SCREEN_WIDTH / 20, SCREEN_HEIGHT / 2 - SCREEN_HEIGHT / 20, SCREEN_WIDTH / 10,
+                    SCREEN_HEIGHT / 10);
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         new Main();
     }
