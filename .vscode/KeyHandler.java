32b72cd966780b309710a66aaf0ac94d7130b2d6
jan Niki <143094436+1Nicky8523@users.noreply.github.com>
2024-04-25 13:22:58 +0000

    keyHandling
    Co-authored-by: CM <cmucsacademy@users.noreply.github.com>
    Co-authored-by: MasterVelyk <MasterVelyk@users.noreply.github.com>

---
+++ .vscode/KeyHandler.java
@@ -0,0 +1,44 @@
+import java.awt.event.*;
+
+public class KeyHandler implements KeyListener {
+    public boolean upPressed = false;
+    public boolean downPressed = false;
+    public boolean rightPressed = false;
+    public boolean leftPressed = false;
+
+    public void keyTyped(KeyEvent e) {
+        // TODO make it work
+        throw new UnsupportedOperationException("Unimplemented method 'keyTyped'");
+    }
+
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
+            upPressed = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
+            downPressed = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A) {
+            leftPressed = true;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D) {
+            rightPressed = true;
+        }
+    }
+
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W) {
+            upPressed = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S) {
+            downPressed = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT || e.getKeyCode() == KeyEvent.VK_A) {
+            leftPressed = false;
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT || e.getKeyCode() == KeyEvent.VK_D) {
+            rightPressed = false;
+        }
+    }
+
+}
--- .vscode/Main.java
+++ .vscode/Main.java
@@ -5,27 +5,43 @@ import javax.swing.*;
 import java.util.ArrayList;
 
 @SuppressWarnings("unused")
-public class Main extends Canvas implements KeyListener, Runnable {
-   private static int SCREEN_WIDTH = 1000;
-   private static final int SCREEN_HEIGHT = 750;
+public class Main extends Canvas implements Runnable {
+   private static final int SCREEN_WIDTH = 1000;
+   private static final int SCREEN_HEIGHT = 1000;
    private static boolean notebookOpen = false;
-   private int direction = -1;
-   private int rectX = 0;
+   private int playerX = 100;
+   private int playerY = 100;
+   private int playerSpeed = 10;
 
    Thread gameThread;
+   KeyHandler keyHandler = new KeyHandler();
 
-   public void keyPressed(KeyEvent e) {
-      if (e.getKeyCode() == KeyEvent.VK_X) {
-         notebookOpen = true;
-      }
+   public void mousePressed(MouseEvent e) {
+
    }
 
-   public void keyTyped(KeyEvent key) {
-   
+   public void mouseReleased(MouseEvent e) {
+
    }
 
-   public void keyReleased(KeyEvent key) {
-   
+   public void mouseExited(MouseEvent e) {
+
+   }
+
+   public void mouseEntered(MouseEvent e) {
+
+   }
+
+   public void mouseClicked(MouseEvent e) {
+
+   }
+
+   public void mouseDragged(MouseEvent e) {
+
+   }
+
+   public void mouseMoved(MouseEvent e) {
+
    }
 
    public Main() {
@@ -36,26 +52,28 @@ public class Main extends Canvas implements KeyListener, Runnable {
       frame.getContentPane().setBackground(Color.DARK_GRAY);
       frame.setVisible(true);
       frame.setResizable(false);
+      frame.addKeyListener(keyHandler);
+      frame.setFocusable(true);
       frame.addWindowListener(
-                new WindowAdapter() {
-                   public void windowClosing(WindowEvent e) {
-                      System.exit(0);
-                   }
-                });
+            new WindowAdapter() {
+               public void windowClosing(WindowEvent e) {
+                  System.exit(0);
+               }
+            });
    }
 
    public void paint(Graphics g) {
       super.paint(g);
-   
-      Graphics2D g2 = (Graphics2D)g;
+
+      Graphics2D g2 = (Graphics2D) g;
       g2.setColor(Color.CYAN);
-      g2.fillRect(rectX, 10, 100, 100);
+      g2.drawOval(playerX, playerY, 40, 40);
    }
 
    public static void main(String[] args) throws Exception {
       new Main();
    }
-   
+
    public void startGameThread() {
       gameThread = new Thread(this);
       gameThread.start();
@@ -63,22 +81,48 @@ public class Main extends Canvas implements KeyListener, Runnable {
 
    public void run() {
       while (gameThread != null) {
-        long currentTime = System.nanoTime();
-        System.out.println("Current Time: "+currentTime);
-        
+         double drawInterval = 1000000000 / 24;
+         double nextDrawInterval = System.nanoTime() + drawInterval;
+
          update();
          repaint();
+
+         try {
+            double remainingTime = nextDrawInterval - System.nanoTime();
+            remainingTime = remainingTime / 1000000;
+            if (remainingTime < 0) {
+               remainingTime = 0;
+            }
+            Thread.sleep((long) remainingTime);
+         } catch (Exception e) {
+         }
       }
    }
 
    public void update() {
-      if (rectX < 0) {
-         direction+=2;
+      if (keyHandler.upPressed == true) {
+         playerY -= playerSpeed;
+         if (playerY <= 0) {
+            playerY = 0;
+         }
       }
-      if (rectX > 1000) {
-         direction-=2;
+      if (keyHandler.downPressed == true) {
+         playerY += playerSpeed;
+         if (playerY + 40 <= SCREEN_HEIGHT) {
+            playerY = SCREEN_HEIGHT - 40;
+         }
       }
-      rectX+=direction;
-      System.out.println(rectX);
+      if (keyHandler.leftPressed == true) {
+         playerX -= playerSpeed;
+         if (playerX <= 0) {
+            playerX = 0;
+         }
+      }
+      if (keyHandler.rightPressed == true) {
+         playerX += playerSpeed;
+         if (playerX + 40 <= SCREEN_WIDTH) {
+            playerX = SCREEN_WIDTH - 40;
+         }
+      }
    }
 }
