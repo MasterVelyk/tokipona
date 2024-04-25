2dcef43a5cc21f1598fcaba65d363945b77e080e
jan Niki <143094436+1Nicky8523@users.noreply.github.com>
2024-04-25 12:15:21 +0000

    aa

--- .vscode/Main.java
+++ .vscode/Main.java
@@ -6,68 +6,79 @@ import java.util.ArrayList;
 
 @SuppressWarnings("unused")
 public class Main extends Canvas implements KeyListener, Runnable {
-    private static int SCREEN_WIDTH = 1000;
-    private static final int SCREEN_HEIGHT = 750;
-    private static boolean notebookOpen = false;
-    private int rectX = 0;
+   private static int SCREEN_WIDTH = 1000;
+   private static final int SCREEN_HEIGHT = 750;
+   private static boolean notebookOpen = false;
+   private int direction = -1;
+   private int rectX = 0;
 
-    Thread gameThread;
+   Thread gameThread;
 
-    public void keyPressed(KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_X) {
-            notebookOpen = true;
-        }
-    }
+   public void keyPressed(KeyEvent e) {
+      if (e.getKeyCode() == KeyEvent.VK_X) {
+         notebookOpen = true;
+      }
+   }
 
-    public void keyTyped(KeyEvent key) {
+   public void keyTyped(KeyEvent key) {
+   
+   }
 
-    }
+   public void keyReleased(KeyEvent key) {
+   
+   }
 
-    public void keyReleased(KeyEvent key) {
-
-    }
-
-    public Main() {
-        startGameThread();
-        JFrame frame = new JFrame("ma pona");
-        frame.add(this);
-        frame.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-        frame.getContentPane().setBackground(Color.DARK_GRAY);
-        frame.setVisible(true);
-        frame.setResizable(false);
-        frame.addWindowListener(
+   public Main() {
+      startGameThread();
+      JFrame frame = new JFrame("ma pona");
+      frame.add(this);
+      frame.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+      frame.getContentPane().setBackground(Color.DARK_GRAY);
+      frame.setVisible(true);
+      frame.setResizable(false);
+      frame.addWindowListener(
                 new WindowAdapter() {
-                    public void windowClosing(WindowEvent e) {
-                        System.exit(0);
-                    }
+                   public void windowClosing(WindowEvent e) {
+                      System.exit(0);
+                   }
                 });
-    }
+   }
 
-    public void paint(Graphics g) {
-        super.paint(g);
+   public void paint(Graphics g) {
+      super.paint(g);
+   
+      Graphics2D g2 = (Graphics2D)g;
+      g2.setColor(Color.CYAN);
+      g2.fillRect(rectX, 10, 100, 100);
+   }
 
-        Graphics2D g2 = (Graphics2D)g;
-        g2.setColor(Color.CYAN);
-        g2.fillRect(rectX, 10, 30, 30);
-    }
+   public static void main(String[] args) throws Exception {
+      new Main();
+   }
+   
+   public void startGameThread() {
+      gameThread = new Thread(this);
+      gameThread.start();
+   }
 
-    public static void main(String[] args) throws Exception {
-        new Main();
-    }
-    
-    public void startGameThread() {
-        gameThread = new Thread(this);
-        gameThread.start();
-    }
+   public void run() {
+      while (gameThread != null) {
+        long currentTime = System.nanoTime();
+        System.out.println("Current Time: "+currentTime);
+        
+         update();
+         repaint();
+      }
+   }
 
-    public void run() {
-        while (gameThread != null) {
-            update();
-            repaint();
-        }
-    }
-
-    public void update() {
-        rectX+=1;
-    }
+   public void update() {
+      if (rectX < 0) {
+         direction+=2;
+      }
+      if (rectX > 1000) {
+         direction-=2;
+      }
+      rectX+=direction;
+      System.out.println(rectX);
+   }
 }
