c33ed045f7325224583c1a98111b973eba938e7a
jan Niki <143094436+1Nicky8523@users.noreply.github.com>
2024-05-16 01:54:27 +0000

    Committing to my branch, got a whole one event working. We need to make a sort method in Masterlist for seenlist that removes duplicate words.

--- .vscode/EventHandler.java
+++ .vscode/EventHandler.java
@@ -1,18 +1,29 @@
+import java.util.ArrayList;
+
 public class EventHandler {
     // holds all the events
-    private Notebook myNotebook;
+    private Masterlist myList;
+    protected boolean hasNextLine = false;
+    protected int myLine = -1;
 
-    public EventHandler(Notebook notebook) {
-        notebook = myNotebook;
+    public EventHandler(Masterlist list) {
+        myList = list;
     }
 
-    public String runEvent(int event, int line) {
+    public ArrayList<Integer> runEvent(int event) {
+      if (hasNextLine = false) {
+         myLine = -1;
+        }
+      myLine+=1;
+      ArrayList<Integer> dialogue = new ArrayList<Integer>();
         // old man dialogue
         if (event == 0) {
-            if (line == 0) {
-                return "toki a";
+            if (myLine == 0) {
+                dialogue.add(119);
+                dialogue.add(0);
+                hasNextLine = false;
             }
         }
-        return "";
+        return dialogue;
     }
 }
