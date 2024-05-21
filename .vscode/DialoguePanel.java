25e3e634d253441d8371c78d01d001d5283fcba9
CM <93284352+cmucsacademy@users.noreply.github.com>
2024-05-21 12:40:44 -0400

    Update DialoguePanel.java

--- .vscode/DialoguePanel.java
+++ .vscode/DialoguePanel.java
@@ -1,35 +1,28 @@
 import java.awt.*;
-import java.awt.event.*;
-import java.awt.geom.AffineTransform;
-import java.awt.image.BufferStrategy;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.util.ArrayList;
 
 public class DialoguePanel extends JPanel {
    public boolean displayDialogue = false;
    public ArrayList<Integer> dialogueSentence = new ArrayList<Integer>();
-   
+
    public DialoguePanel() {
       this.setPreferredSize(new Dimension(Main.SCREEN_WIDTH, 100));
-      this.setBackground(new Color(115, 115, 115));
+      this.setBackground(new Color(223, 189, 159));
    }
-   
-   
+
    public void paint(Graphics g) {
       super.paint(g);
       Graphics2D g2 = (Graphics2D) g;
-      
+
       if (displayDialogue) {
          for (int i = 0; i < dialogueSentence.size(); i++) {
-            g2.drawImage(Masterlist.masterlist[dialogueSentence.get(i).intValue()].sitelen, 100+50*i, 25, 50, 50, null);
+            g2.drawImage(Masterlist.masterlist[dialogueSentence.get(i).intValue()].sitelen, 100 + 50 * i, 25, 50, 50,
+                  null);
          }
       }
-      
+
+      g2.setColor(new Color(178, 128, 79));
       g2.setStroke(new BasicStroke(5));
       g2.draw(new Rectangle(10, 10, 80, 80));
       g2.fillOval(25, 45, 10, 10);
