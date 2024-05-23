2efe31f8a3f04d278a45f2dd1a6daccb4ccbd371
CM <93284352+cmucsacademy@users.noreply.github.com>
2024-05-23 17:21:53 -0400

    Update Sentence.java

--- .vscode/Sentence.java
+++ .vscode/Sentence.java
@@ -1,32 +1,36 @@
 import java.util.ArrayList;
 
 public class Sentence {
-   private ArrayList<Word> words = new ArrayList<Word>();
+    private ArrayList<Word> words = new ArrayList<Word>();
 
-   public Sentence(ArrayList<Word> w) {
-      words = w;
-   }
+    public Sentence(ArrayList<Word> w) {
+        words = w;
+    }
     
-   public Sentence() {
-   }
+    public Sentence() {
+    }
 
-   public ArrayList<Word> getSentence() {
-      return words;
-   }
+    public Sentence(Word w) {
+        words.add(w);
+    }
 
-   public Word get(int index) {
-      return words.get(index);
-   }
+    public ArrayList<Word> getSentence() {
+        return words;
+    }
 
-   public void add(Word w) {
-      words.add(w);
-   }
+    public Word get(int index) {
+        return words.get(index);
+    }
+
+    public void add(Word w) {
+        words.add(w);
+    }
     
-   public void clear() {
+    public void clear() {
       words.clear();
-   }
+    }
     
-   public boolean equals(Sentence other) {
+    public boolean equals(Sentence other) {
       for (int i = 0; i < this.getSentence().size(); i++) {
          if (i < this.getSentence().size() && i < other.getSentence().size()) { 
             if (!(this.getSentence().get(i).getToki().equals(other.getSentence().get(i).getToki()))) {
@@ -38,5 +42,5 @@ public class Sentence {
          }
       }
       return true;
-   }
+    }
 }
