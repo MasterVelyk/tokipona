bc7ff5fc152c755c13e549bd5e96b3c7fadedec2
CM <93284352+cmucsacademy@users.noreply.github.com>
2024-05-23 17:17:13 -0400

    Update NotebookPage.java

--- .vscode/NotebookPage.java
+++ .vscode/NotebookPage.java
@@ -5,58 +5,57 @@ import java.util.ArrayList;
 import javax.imageio.ImageIO;
 
 public class NotebookPage {
-   private BufferedImage img = null;
-   private Sentence answer = new Sentence(); // answer for the page
-   private Sentence guess = new Sentence(); // guess for the page
-   protected String imageID = "";
-   protected boolean completed = false;
+    private BufferedImage img = null;
+    private Sentence answer = new Sentence(); // answer for the page
+    private Sentence guess = new Sentence(); // guess for the page
+    protected String imageID = "";
+    protected boolean completed = false;
 
-   public NotebookPage(Sentence ans, String image) {
-      try {
-         img = ImageIO.read(getClass().getResource("/images/" + image + ".png"));
-      } catch (IOException e) {
-         System.out.println(e);
-      }
-      imageID = image;
-      answer = ans;
-   }
+    public NotebookPage(Sentence ans, String image) {
+        try {
+            img = ImageIO.read(getClass().getResource("/images/" + image + ".png"));
+        } catch (IOException e) {
+        }
+        imageID = image;
+        answer = ans;
+    }
 
-   public NotebookPage() {
-      try {
-         img = ImageIO.read(getClass().getResource("/images/transparent.png"));
-      } catch (IOException e) {
-      }
-      imageID = "transparent";
-      ArrayList<Word> answerArray = new ArrayList<Word>();
-      answer = new Sentence(answerArray);
-   }
+    public NotebookPage() {
+        try {
+            img = ImageIO.read(getClass().getResource("/images/transparent.png"));
+        } catch (IOException e) {
+        }
+        imageID = "transparent";
+        ArrayList<Word> answerArray = new ArrayList<Word>();
+        answer = new Sentence(answerArray);
+    }
 
-   public BufferedImage getImage() {
-      return img;
-   }
+    public BufferedImage getImage() {
+        return img;
+    }
 
-   public Sentence getAnswer() {
-      return answer;
-   }
+    public Sentence getAnswer() {
+        return answer;
+    }
 
-   public Sentence getGuess() {
-      return guess;
-   }
+    public Sentence getGuess() {
+        return guess;
+    }
 
-   public boolean checkGuess() {
-      if (answer.equals(guess)) {
-         completed = true;
-         return true;
-      }
-      return false;
-   }
+    public boolean checkGuess() {
+        if (answer.equals(guess)) {
+            completed = true;
+            return true;
+        }
+        return false;
+    }
 
-   public void writeGuess(ArrayList<Word> g) {
-      guess.clear();
-      for (int i = 0; i < answer.getSentence().size(); i++) {
-         if (i < g.size()) {
-            guess.add(g.get(i));
-         }
-      }
-   }
+    public void writeGuess(ArrayList<Word> g) {
+        guess.clear();
+        for (int i = 0; i < answer.getSentence().size(); i++) {
+            if (i < g.size()) {
+                guess.add(g.get(i));
+            }
+        }
+    }
 }
